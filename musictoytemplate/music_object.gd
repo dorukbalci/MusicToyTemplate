48c86d4348f16a8d30f3a80024e3ae60e9108539
Doruk Balci <doruk.balci@york.ac.uk>
2025-03-07 23:48:10 +0000

    Input and Sound

---
+++ musictoytemplate/music_object.gd
@@ -0,0 +1,17 @@
+extends Node2D
+@export var available_keys: Array[String] = ["Q", "W", "E"]  # Define available actions
+@export var selected_key: String = "Q"  # Select which action to use
+
+@export var audio_file: Resource
+@export var animation: Animation
+
+func _ready() -> void:
+	$AudioStreamPlayer2D.stream = audio_file
+	
+
+func _input(event):
+	if selected_key != "" and event.is_action_pressed(selected_key):
+		$AudioStreamPlayer2D.play()
+	elif selected_key != "" and event.is_action_released(selected_key):
+		$AudioStreamPlayer2D.stop()
+		
