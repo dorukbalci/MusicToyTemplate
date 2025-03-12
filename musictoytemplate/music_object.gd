4f87a04252ef54438e6ca56b4401511567ace6cf
Doruk Balci <doruk.balci@york.ac.uk>
2025-03-12 19:18:55 +0000

    InputMap update

--- musictoytemplate/music_object.gd
+++ musictoytemplate/music_object.gd
@@ -5,9 +5,23 @@ extends Node2D
 @export var audio_file: Resource
 @export var animation: Animation
 
+
+@export_range(0, 10) var volume: float = 1:  
+	set = set_volume  # Updates the volume when changed in the editor
 func _ready() -> void:
 	$AudioStreamPlayer2D.stream = audio_file
-	
+
+# Converts a value (0-100) to decibels
+func linear_to_db(value: float) -> float:
+	if value <= 0:
+		return -80.0  # Silence
+	return lerpf(-80.0, 0.0, value / 100.0)
+
+# Sets the volume and updates the AudioStreamPlayer
+func set_volume(value: float):
+	volume = value  # Store the value
+	if $AudioStreamPlayer2D:
+		$AudioStreamPlayer2D.volume_db = linear_to_db(value)
 
 func _input(event):
 	if selected_key != "" and event.is_action_pressed(selected_key):
